d67be719a8aed62d8889925b56d16a0e1a98b896
Prateek yadav <shubhamy9451@gmail.com>
2022-08-08 20:46:00 +0530

    adding start btn

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const hbs=require('hbs');
-const port= process.env.port||4000;
+const port= process.env.PORT||8000;
 
 // public static path
 const static_path=path.join(__dirname,"../public");
